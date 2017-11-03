7709951db7d0eeea11db19792f1e6287813a3b54
Rafael Lucas Chagas <rafael.l.chagas@gmail.com>
2017-11-03 20:35:40 -0200

    Update nbaWebScraping.py

--- nbaWebScraping.py
+++ nbaWebScraping.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-
 def getPlayerStats(url,years):
-    
+    #funcao para recuperar dados danba na web
     getUrl = url + str("year=") + str(years)
     response = requests.get(getUrl)
     soup = BeautifulSoup(response.text, 'lxml')
@@ -11,21 +10,19 @@ def getPlayerStats(url,years):
     df = pd.read_html(str(table))[0]
     team = []
     player = []
-    
+    #loop para adicionar o campo team
     for j in df['Player']:
         j = j.split('(')
         player.append(j[0])
         team.append(j[1])
-    
     team = pd.Series(team).str.replace(')', '')
-    year = pd.Series(years)
     df['Player'] = player
     df['Team'] = team
     df['Year']= years
     return df
 
 def getHist(dfIn,time):
-    
+    #funcao para fazer a carga dos dados historicos
     for getData in range(len(time)):
         temp = getPlayerStats(myUrl,time[getData]) 
         dfIn = dfIn.append(temp, ignore_index=True) 
